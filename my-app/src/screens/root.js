65fea881cebbd7a07ac875fdb2babc6ccd0f1a12
Kurosaki Aki <kurosakiaki890@gmail.com>
2022-03-05 17:00:40 +0700

    guide all

--- my-app/src/screens/root.js
+++ my-app/src/screens/root.js
@@ -2,12 +2,18 @@ import React from "react"
 import {Routes, Route } from "react-router-dom"
 import Home from "../components/Home"
 import Contact from "../components/Contact"
-
+import Guide from "../components/Guide"
+import GuideConnectCamera from "../components/GuideConnectCamera"
+import GuideReportDefect from "../components/GuideReportDefect"
 const ScreensRoot  = () => {
     return (
       <Routes>  
         <Route path="/"        element={ <Home /> } />  
         <Route path="/contact" element={ <Contact />} />
+        <Route path="/guide" element={ <Guide />} />
+        <Route path="/connect-camera" element={ <GuideConnectCamera />} />
+        <Route path="/report-defect" element={ <GuideReportDefect />} />
+
       </Routes>
     )
 }
