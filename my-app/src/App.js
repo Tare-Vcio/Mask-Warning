7452bebc7ed1cedbc3ed40a50b66f9a64699cedc
levanthuan2001qttl <81482705+levanthuan2001qttl@users.noreply.github.com>
2022-03-07 16:55:48 +0700

    merge3

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import ScreensRoot from "./screens/root";
-
+//hello
 function App() {
   return (
     <BrowserRouter> 
